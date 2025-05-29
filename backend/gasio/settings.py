c7c511db3e248f4d7fb319f046a66291a8f5ad21
Diego Richalski <diego.richalski@gmail.com>
2025-05-29 19:18:28 -0300

    change allowed_hosts

--- backend/gasio/settings.py
+++ backend/gasio/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r+8b#epk7z*_0$#c2-%#46v)jnxv7(jg*x*6@o1qpi-vq)2!7j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
