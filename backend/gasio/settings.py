3a612795c6c36667a311e00ccf7dac21c75d7672
Diego Richalski <diego.richalski@gmail.com>
2025-06-05 13:46:48 -0300

    Hard change on settings

--- backend/gasio/settings.py
+++ backend/gasio/settings.py
@@ -128,8 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
